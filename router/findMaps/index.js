bce8cddb9bc211f83b28c8aac5d20d46f093759c
wonseok22 <84431483+wonseok22@users.noreply.github.com>
2022-01-26 17:41:08 +0900

    2022-01-26
    add header

---
+++ router/findMaps/index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+var router = express.Router()
+var path = require('path')
+
+
+router.get('/', function(req, res) {
+	res.render('findmap.ejs');
+})
+
+
+module.exports = router;
--- router/index.js
+++ router/index.js
@@ -2,13 +2,14 @@ var express = require('express');
 var app = express();
 var router = express.Router();
 var path = require('path');
-
-
+var findMaps = require('./findMaps/index')
+;
 //url root
 router.get('/', function(req,res){
     res.sendfile( path.join(__dirname, "../public/main.html"))
 });
 
+router.use('/findMaps', findMaps);
 
 module.exports = router;
 
