f292834faa2d5ecaa47ac16babed8ef149db6c45
wonseok22 <84431483+wonseok22@users.noreply.github.com>
2022-01-27 17:33:28 +0900

    2022-01-27

--- router/findMaps/index.js
+++ router/findMaps/index.js
@@ -9,13 +9,11 @@ const authKey = "1d070a748c594769afd68d31bcb3c6ff";
 const city = 41270;
 
 router.get('/', function(req, res) {
-
 	res.render('findmap.ejs');
 })
 
 const resPharmacy = function(authKey){  // 동물약국 좌표 받아오기
 	var urls = "https://openapi.gg.go.kr/AnimalPharmacy";
-	var pharmacyDatas = [];
 	const option = {
 		KEY:authKey,
 		Type: "json",
@@ -24,20 +22,15 @@ const resPharmacy = function(authKey){  // 동물약국 좌표 받아오기
 		SIGUN_CD: city
 	}
 	request.get({
-	uri: urls,
-	qs :option,
+		uri: urls,
+		qs :option,
 	}, function(err, res, body) {
 		let json = JSON.parse(body) //json으로 파싱
-		for (let i=0; i<json.AnimalPharmacy[1].row.length; i++){
-			pharmacyDatas.push([json.AnimalPharmacy[1].row[i].REFINE_WGS84_LOGT, json.AnimalPharmacy[1].row[i].REFINE_WGS84_LAT])
-		}
-		return pharmacyDatas
 	})
 }
 
 const resHospital = function(authKey){ // 동물병원 좌표 받아오기 
 	let urls = "https://openapi.gg.go.kr/Animalhosptl";
-	var hospitalDatas = [];
 	const option = {
 		KEY:authKey,
 		Type: "json",
@@ -46,15 +39,11 @@ const resHospital = function(authKey){ // 동물병원 좌표 받아오기
 		SIGUN_CD: city
 	}
 	request.get({
-	uri: urls,
-	qs :option,
+		uri: urls,
+		qs :option,
 	}, function(err, res, body) {
 		let json = JSON.parse(body) //json으로 파싱
-		for (let i=0; i<json.Animalhosptl[1].row.length; i++){
-			hospitalDatas.push([json.Animalhosptl[1].row[i].REFINE_WGS84_LOGT, json.Animalhosptl[1].row[i].REFINE_WGS84_LAT])
-		}
-		return hospitalDatas
 	})
-	
 }
+
 module.exports = router;
