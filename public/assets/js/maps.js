3192f79af1f095982d59d499b93cec028dc34c31
wonseok22 <84431483+wonseok22@users.noreply.github.com>
2022-01-27 16:34:00 +0900

    2022-01-27

--- public/assets/js/maps.js
+++ public/assets/js/maps.js
@@ -1,3 +1,4 @@
+
 var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
 var options = { //지도를 생성할 때 필요한 기본 옵션
 	center: new kakao.maps.LatLng(37.3183169898, 126.8352), //지도의 중심좌표.
--- router/findMaps/index.js
+++ router/findMaps/index.js
@@ -2,11 +2,59 @@ var express = require('express')
 var app = express()
 var router = express.Router()
 var path = require('path')
+var request = require('request')
+var cheerio = require('cheerio')
 
+const authKey = "1d070a748c594769afd68d31bcb3c6ff";
+const city = 41270;
 
 router.get('/', function(req, res) {
+
 	res.render('findmap.ejs');
 })
 
+const resPharmacy = function(authKey){  // 동물약국 좌표 받아오기
+	var urls = "https://openapi.gg.go.kr/AnimalPharmacy";
+	var pharmacyDatas = [];
+	const option = {
+		KEY:authKey,
+		Type: "json",
+		pIndex: "1",
+		pSize: '100',
+		SIGUN_CD: city
+	}
+	request.get({
+	uri: urls,
+	qs :option,
+	}, function(err, res, body) {
+		let json = JSON.parse(body) //json으로 파싱
+		for (let i=0; i<json.AnimalPharmacy[1].row.length; i++){
+			pharmacyDatas.push([json.AnimalPharmacy[1].row[i].REFINE_WGS84_LOGT, json.AnimalPharmacy[1].row[i].REFINE_WGS84_LAT])
+		}
+		return pharmacyDatas
+	})
+}
 
+const resHospital = function(authKey){ // 동물병원 좌표 받아오기 
+	let urls = "https://openapi.gg.go.kr/Animalhosptl";
+	var hospitalDatas = [];
+	const option = {
+		KEY:authKey,
+		Type: "json",
+		pIndex: "1",
+		pSize: '100',
+		SIGUN_CD: city
+	}
+	request.get({
+	uri: urls,
+	qs :option,
+	}, function(err, res, body) {
+		let json = JSON.parse(body) //json으로 파싱
+		for (let i=0; i<json.Animalhosptl[1].row.length; i++){
+			hospitalDatas.push([json.Animalhosptl[1].row[i].REFINE_WGS84_LOGT, json.Animalhosptl[1].row[i].REFINE_WGS84_LAT])
+		}
+		return hospitalDatas
+	})
+	
+}
 module.exports = router;
