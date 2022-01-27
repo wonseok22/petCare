2d223ebfa60c02edce143ea84c1084dbd9ce9e4f
wonseok22 <84431483+wonseok22@users.noreply.github.com>
2022-01-27 20:41:33 +0900

    2022-01-27

--- router/findMaps/index.js
+++ router/findMaps/index.js
@@ -6,44 +6,54 @@ var request = require('request')
 var cheerio = require('cheerio')
 
 const authKey = "1d070a748c594769afd68d31bcb3c6ff";
+const phaUrls = "https://openapi.gg.go.kr/AnimalPharmacy";
+const hosUrls = "https://openapi.gg.go.kr/Animalhosptl";
 const city = 41270;
+let phaData = 1, 
+	hosData = 2;
 
-router.get('/', function(req, res) {
-	res.render('findmap.ejs');
+router.get('/', (req, res) => {
+	var pRes = res;
+	const option = {
+		KEY:authKey,
+		Type: "json",
+		pIndex: "1",
+		pSize: '100',
+		SIGUN_CD: city
+	}
+	
+	request.get({
+		uri: phaUrls,
+		qs :option,
+	}, function(err, res, body) {
+		phaData = JSON.parse(body) //json으로 파싱
+		let logt = {};
+		let lat = {};
+		let name = {};
+		for (let i=0; i<phaData.AnimalPharmacy[1].row.length; i++){
+			logt[i]=phaData.AnimalPharmacy[1].row[i].REFINE_WGS84_LOGT
+			lat[i]=phaData.AnimalPharmacy[1].row[i].REFINE_WGS84_LAT
+			name[i]=phaData.AnimalPharmacy[1].row[i].BIZPLC_NM
+		}
+		let data = {
+			logt : logt,
+			lat : lat,
+			name : name
+		}
+		pRes.render('findmap.ejs', data);
+		//console.log(phaData.AnimalPharmacy[1].row)
+	})
+	// console.log(phaData)
+	// request.get({
+	// 	uri: hosUrls,
+	// 	qs :option,
+	// }, function(err, res, body) {
+	// 	hosData = JSON.parse(body) //json으로 파싱
+	// })
+	// console.log(phaData)
+	// res.render('findmap.ejs');
 })
 
-const resPharmacy = function(authKey){  // 동물약국 좌표 받아오기
-	var urls = "https://openapi.gg.go.kr/AnimalPharmacy";
-	const option = {
-		KEY:authKey,
-		Type: "json",
-		pIndex: "1",
-		pSize: '100',
-		SIGUN_CD: city
-	}
-	request.get({
-		uri: urls,
-		qs :option,
-	}, function(err, res, body) {
-		let json = JSON.parse(body) //json으로 파싱
-	})
-}
 
-const resHospital = function(authKey){ // 동물병원 좌표 받아오기 
-	let urls = "https://openapi.gg.go.kr/Animalhosptl";
-	const option = {
-		KEY:authKey,
-		Type: "json",
-		pIndex: "1",
-		pSize: '100',
-		SIGUN_CD: city
-	}
-	request.get({
-		uri: urls,
-		qs :option,
-	}, function(err, res, body) {
-		let json = JSON.parse(body) //json으로 파싱
-	})
-}
 
 module.exports = router;
